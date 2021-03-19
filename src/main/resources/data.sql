a9c115cfe94bb0264b2367b0d2037c45a33c2fb2
yuranous <garkavy_yuri@mail.ru>
2021-03-19 13:27:04 +0300

    Created new schema.sql and data.sql files for new project DB schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,61 @@
-INSERT INTO House (id, version, address) VALUES (1, 0, 'ул.Цюрупы, 16');
+-- Вставка типов документов
+INSERT INTO Document (id, version, code, name)
+VALUES (1, 0, 3, 'Свидетельство о рождении');
+INSERT INTO Document (id, version, code, name)
+VALUES (2, 0, 7, 'Военный билет ');
+INSERT INTO Document (id, version, code, name)
+VALUES (3, 0, 8, 'Временное удостоверение, выданное взамен военного билета  ');
+INSERT INTO Document (id, version, code, name)
+VALUES (4, 0, 10, 'Паспорт иностранного гражданина');
+INSERT INTO Document (id, version, code, name)
+VALUES (5, 0, 11,
+        'Свидетельство о рассмотрении ходатайства о признании ' ||
+        'лица беженцем на территории Российской Федерации по существу');
+INSERT INTO Document (id, version, code, name)
+VALUES (6, 0, 12, 'Вид на жительство в Российской Федерации');
+INSERT INTO Document (id, version, code, name)
+VALUES (7, 0, 13, 'Удостоверение беженца');
+INSERT INTO Document (id, version, code, name)
+VALUES (8, 0, 15, 'Разрешение на временное проживание в Российской Федерации');
+INSERT INTO Document (id, version, code, name)
+VALUES (9, 0, 18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
+INSERT INTO Document (id, version, code, name)
+VALUES (10, 0, 21, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Document (id, version, code, name)
+VALUES (11, 0, 23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
+INSERT INTO Document (id, version, code, name)
+VALUES (12, 0, 24, 'Удостоверение личности военнослужащего Российской Федерации');
+INSERT INTO Document (id, version, code, name)
+VALUES (13, 0, 91, 'Иные документы');
 
-INSERT INTO House (id, version, address) VALUES (2, 0, 'ул.Лунина, 7');
+--Вставка стран
+INSERT INTO Country (id, version, code, name)
+VALUES (1, 0, 634, 'Российская Федерация');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (1, 0, 'Пётр', 20);
+--Вставка организаций
+INSERT
+INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
+VALUES (1, 0, 'СБ', 'СберБанк', '123456789012', '123456789', 'ул. Тверская, 1', '8005553535', true);
+INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
+VALUES (2, 0, 'BI', 'Bell Integrator', '210987654321', '987654321', 'ул. Пушкинская, 2', '4999996565', true);
 
-INSERT INTO Person (id, version, first_name, age) VALUES (2, 0, 'John', 25);
+--Вставка офисов
+INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
+VALUES (1, 0, 'Рассвет', '4950765698', 'п. Столярный, 3', true, 2);
+INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
+VALUES (2, 0, 'Восход', '4950735692', 'п. Столярный, 10', true, 2);
+INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
+VALUES (3, 0, 'Мир', '4990835628', 'б. Новинский, 32', true, 1);
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 1);
+--Вставка пользователей
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, position, phone,
+                  doc_code, doc_number, doc_date, citizenship_code, is_identified)
+VALUES (1, 0, 1, 'Геннадий', 'Геннадиевич', 'Горин', 'Менеджер', '9995553535',
+        21, '555555555555', CURRENT_DATE(), 634, true);
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, position,
+                  doc_code, doc_number, doc_date, citizenship_code, is_identified)
+VALUES (2, 0, 1, 'Цветогор', 'Грайрович', 'Петрухин', 'Аналитик',
+        21, '555555555555', CURRENT_DATE(), 634, true);
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 2);
+INSERT INTO User (office_id, first_name, position)
+VALUES (3, 'Лесоруб', 'Разработчик');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,69 @@
-CREATE TABLE IF NOT EXISTS Person (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    first_name VARCHAR(50) NOT NULL COMMENT 'Имя',
-    age        INTEGER  NOT NULL    COMMENT 'Возраст'
+CREATE TABLE IF NOT EXISTS Document
+(
+    id      INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    code    INTEGER NOT NULL UNIQUE COMMENT 'Код',
+    name    VARCHAR(150) COMMENT 'Наименование'
 );
-COMMENT ON TABLE Person IS 'Человек';
+COMMENT ON TABLE Document IS 'Документ';
 
-CREATE TABLE IF NOT EXISTS House (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
+CREATE TABLE IF NOT EXISTS Country
+(
+    id      INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    code    INTEGER NOT NULL UNIQUE COMMENT 'Код',
+    name    VARCHAR(25) COMMENT 'Наименование'
 );
-COMMENT ON TABLE House IS 'Дом';
+COMMENT ON TABLE Country IS 'Страна';
 
-CREATE TABLE IF NOT EXISTS Person_House (
-    person_id   INTEGER  NOT NULL COMMENT 'Уникальный идентификатор человека',
-    house_id    INTEGER  NOT NULL COMMENT 'Уникальный идентификатор дома',
-
-    PRIMARY KEY (person_id, house_id)
+CREATE TABLE IF NOT EXISTS Organization
+(
+    id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version   INTEGER NOT NULL DEFAULT 0      NOT NULL COMMENT 'Служебное поле hibernate',
+    name      VARCHAR(25)  NOT NULL COMMENT 'Сокращённое наименование',
+    full_name VARCHAR(250) NOT NULL COMMENT 'Полное наименование',
+    inn       CHAR(12)     NOT NULL COMMENT 'ИНН',
+    kpp       CHAR(9)      NOT NULL COMMENT 'КПП',
+    address   VARCHAR(50)  NOT NULL COMMENT 'Адрес',
+    phone     CHAR(10) COMMENT 'Номер телефона',
+    is_active BOOLEAN DEFAULT TRUE COMMENT 'Статус'
 );
-COMMENT ON TABLE Person_House IS 'join-таблица для связи человека и дома';
+COMMENT ON TABLE Organization IS 'Организация';
 
-CREATE INDEX IX_Person_House_Id ON Person_House (house_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (house_id) REFERENCES House(id);
+CREATE TABLE IF NOT EXISTS Office
+(
+    id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version   INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    name      VARCHAR(25) COMMENT 'Наименование',
+    phone     CHAR(10) COMMENT 'Номер телефона',
+    address   VARCHAR(50) COMMENT 'Адрес',
+    is_active BOOLEAN DEFAULT TRUE COMMENT 'Статус',
+    org_id    INTEGER NOT NULL COMMENT 'Индефикатор организации',
+    CONSTRAINT fk_organization_office FOREIGN KEY (org_id)
+        REFERENCES Organization (id)
+);
+COMMENT ON TABLE Office IS 'Офис';
 
-CREATE INDEX IX_House_Person_Id ON Person_House (person_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (person_id) REFERENCES Person(id);
+CREATE TABLE IF NOT EXISTS User
+(
+    id               INTEGER     NOT NULL COMMENT 'Уникальный идентификатор пользователя' PRIMARY KEY AUTO_INCREMENT,
+    version          INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    office_id        INTEGER     NOT NULL COMMENT 'Офис',
+    CONSTRAINT fk_person_office FOREIGN KEY (office_id)
+        REFERENCES Office (id),
+    first_name       VARCHAR(25) NOT NULL COMMENT 'Имя',
+    second_name      VARCHAR(25) COMMENT 'Фамилия',
+    middle_name      VARCHAR(25) COMMENT 'Отчество',
+    position         VARCHAR(25) NOT NULL COMMENT 'Должнсть',
+    phone            CHAR(10) COMMENT 'Номер телефона',
+    doc_code         INTEGER COMMENT 'Тип документа',
+    CONSTRAINT fk_user_doc_code FOREIGN KEY (doc_code)
+        REFERENCES Document (code),
+    doc_number       VARCHAR(25) COMMENT 'Номер документа',
+    doc_date         DATE COMMENT 'Дата выдачи документа',
+    citizenship_code INTEGER COMMENT 'Код страны',
+    CONSTRAINT fk_user_citizenship_code FOREIGN KEY (citizenship_code)
+        REFERENCES Country (code),
+    is_identified    BOOLEAN DEFAULT TRUE COMMENT 'Статус'
+);
+COMMENT ON TABLE User IS 'Пользователь';

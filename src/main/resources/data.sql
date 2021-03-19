48f3422df7c1886689473bab94518b62874a63d0
yuranous <garkavy_yuri@mail.ru>
2021-03-19 18:06:19 +0300

    Updated schema.sql and data.sql files due to mistyping and updated requirements

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,61 +1,65 @@
 -- Вставка типов документов
-INSERT INTO Document (id, version, code, name)
-VALUES (1, 0, 3, 'Свидетельство о рождении');
-INSERT INTO Document (id, version, code, name)
-VALUES (2, 0, 7, 'Военный билет ');
-INSERT INTO Document (id, version, code, name)
-VALUES (3, 0, 8, 'Временное удостоверение, выданное взамен военного билета  ');
-INSERT INTO Document (id, version, code, name)
-VALUES (4, 0, 10, 'Паспорт иностранного гражданина');
-INSERT INTO Document (id, version, code, name)
-VALUES (5, 0, 11,
+INSERT INTO Document_type (code, name)
+VALUES (3, 'Свидетельство о рождении');
+INSERT INTO Document_type (code, name)
+VALUES (7, 'Военный билет ');
+INSERT INTO Document_type (code, name)
+VALUES (8, 'Временное удостоверение, выданное взамен военного билета  ');
+INSERT INTO Document_type (code, name)
+VALUES (10, 'Паспорт иностранного гражданина');
+INSERT INTO Document_type (code, name)
+VALUES (11,
         'Свидетельство о рассмотрении ходатайства о признании ' ||
         'лица беженцем на территории Российской Федерации по существу');
-INSERT INTO Document (id, version, code, name)
-VALUES (6, 0, 12, 'Вид на жительство в Российской Федерации');
-INSERT INTO Document (id, version, code, name)
-VALUES (7, 0, 13, 'Удостоверение беженца');
-INSERT INTO Document (id, version, code, name)
-VALUES (8, 0, 15, 'Разрешение на временное проживание в Российской Федерации');
-INSERT INTO Document (id, version, code, name)
-VALUES (9, 0, 18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
-INSERT INTO Document (id, version, code, name)
-VALUES (10, 0, 21, 'Паспорт гражданина Российской Федерации');
-INSERT INTO Document (id, version, code, name)
-VALUES (11, 0, 23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
-INSERT INTO Document (id, version, code, name)
-VALUES (12, 0, 24, 'Удостоверение личности военнослужащего Российской Федерации');
-INSERT INTO Document (id, version, code, name)
-VALUES (13, 0, 91, 'Иные документы');
+INSERT INTO Document_type (code, name)
+VALUES (12, 'Вид на жительство в Российской Федерации');
+INSERT INTO Document_type (code, name)
+VALUES (13, 'Удостоверение беженца');
+INSERT INTO Document_type (code, name)
+VALUES (15, 'Разрешение на временное проживание в Российской Федерации');
+INSERT INTO Document_type (code, name)
+VALUES (18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
+INSERT INTO Document_type (code, name)
+VALUES (21, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Document_type (code, name)
+VALUES (23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
+INSERT INTO Document_type (code, name)
+VALUES (24, 'Удостоверение личности военнослужащего Российской Федерации');
+INSERT INTO Document_type (code, name)
+VALUES (91, 'Иные документы');
 
 --Вставка стран
-INSERT INTO Country (id, version, code, name)
-VALUES (1, 0, 634, 'Российская Федерация');
+INSERT INTO Country (code, name)
+VALUES (634, 'Российская Федерация');
 
 --Вставка организаций
 INSERT
-INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
-VALUES (1, 0, 'СБ', 'СберБанк', '123456789012', '123456789', 'ул. Тверская, 1', '8005553535', true);
-INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
-VALUES (2, 0, 'BI', 'Bell Integrator', '210987654321', '987654321', 'ул. Пушкинская, 2', '4999996565', true);
+INTO Organization (name, full_name, inn, kpp, address, phone, is_active)
+VALUES ('СБ', 'СберБанк', '123456789012', '123456789', 'ул. Тверская, 1', '8005553535', true);
+INSERT INTO Organization (name, full_name, inn, kpp, address, phone, is_active)
+VALUES ('BI', 'Bell Integrator', '210987654321', '987654321', 'ул. Пушкинская, 2', '4999996565', true);
 
 --Вставка офисов
-INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
-VALUES (1, 0, 'Рассвет', '4950765698', 'п. Столярный, 3', true, 2);
-INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
-VALUES (2, 0, 'Восход', '4950735692', 'п. Столярный, 10', true, 2);
-INSERT INTO Office (id, version, name, phone, address, is_active, org_id)
-VALUES (3, 0, 'Мир', '4990835628', 'б. Новинский, 32', true, 1);
+INSERT INTO Office (name, phone, address, is_active, org_id)
+VALUES ('Рассвет', '4950765698', 'п. Столярный, 3', true, 2);
+INSERT INTO Office (name, phone, address, is_active, org_id)
+VALUES ('Восход', '4950735692', 'п. Столярный, 10', true, 2);
+INSERT INTO Office (name, phone, address, is_active, org_id)
+VALUES ('Мир', '4990835628', 'б. Новинский, 32', true, 1);
+
+--Вставка документов
+INSERT INTO Document (doc_type_id, doc_number, doc_date)
+VALUES (10, '555555555555', CURRENT_DATE());
+INSERT INTO Document (doc_type_id, doc_number, doc_date)
+VALUES (10, '444444444444', CURRENT_DATE());
 
 --Вставка пользователей
-INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, position, phone,
-                  doc_code, doc_number, doc_date, citizenship_code, is_identified)
-VALUES (1, 0, 1, 'Геннадий', 'Геннадиевич', 'Горин', 'Менеджер', '9995553535',
-        21, '555555555555', CURRENT_DATE(), 634, true);
-INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, position,
-                  doc_code, doc_number, doc_date, citizenship_code, is_identified)
-VALUES (2, 0, 1, 'Цветогор', 'Грайрович', 'Петрухин', 'Аналитик',
-        21, '555555555555', CURRENT_DATE(), 634, true);
+INSERT INTO User (office_id, first_name, second_name, middle_name,
+                  position, doc_id, phone, citizenship_code, is_identified)
+VALUES (1, 'Геннадий', 'Геннадиевич', 'Горин', 'Менеджер', 1, '9995553535', 1, true);
+INSERT INTO User (office_id, first_name, second_name, middle_name,
+                  position, doc_id, citizenship_code, is_identified)
+VALUES (1, 'Цветогор', 'Грайрович', 'Петрухин', 'Аналитик', 2, 1, true);
 
 INSERT INTO User (office_id, first_name, position)
 VALUES (3, 'Лесоруб', 'Разработчик');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,21 @@
-CREATE TABLE IF NOT EXISTS Document
+CREATE TABLE IF NOT EXISTS Document_type
 (
     id      INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
     version INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
     code    INTEGER NOT NULL UNIQUE COMMENT 'Код',
     name    VARCHAR(150) COMMENT 'Наименование'
+);
+COMMENT ON TABLE Document_type IS 'Тип документа';
+
+CREATE TABLE IF NOT EXISTS Document
+(
+    id         INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    doc_type_id  INTEGER COMMENT 'Тип',
+    CONSTRAINT fk_user_doc_type FOREIGN KEY (doc_type_id)
+        REFERENCES Document_type (id),
+    doc_number VARCHAR(25) COMMENT 'Номер',
+    doc_date   DATE COMMENT 'Дата выдачи'
 );
 COMMENT ON TABLE Document IS 'Документ';
 
@@ -19,14 +31,14 @@ COMMENT ON TABLE Country IS 'Страна';
 CREATE TABLE IF NOT EXISTS Organization
 (
     id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-    version   INTEGER NOT NULL DEFAULT 0      NOT NULL COMMENT 'Служебное поле hibernate',
+    version   INTEGER      NOT NULL DEFAULT 0 NOT NULL COMMENT 'Служебное поле hibernate',
     name      VARCHAR(25)  NOT NULL COMMENT 'Сокращённое наименование',
     full_name VARCHAR(250) NOT NULL COMMENT 'Полное наименование',
     inn       CHAR(12)     NOT NULL COMMENT 'ИНН',
     kpp       CHAR(9)      NOT NULL COMMENT 'КПП',
     address   VARCHAR(50)  NOT NULL COMMENT 'Адрес',
     phone     CHAR(10) COMMENT 'Номер телефона',
-    is_active BOOLEAN DEFAULT TRUE COMMENT 'Статус'
+    is_active BOOLEAN               DEFAULT TRUE COMMENT 'Статус'
 );
 COMMENT ON TABLE Organization IS 'Организация';
 
@@ -37,8 +49,8 @@ CREATE TABLE IF NOT EXISTS Office
     name      VARCHAR(25) COMMENT 'Наименование',
     phone     CHAR(10) COMMENT 'Номер телефона',
     address   VARCHAR(50) COMMENT 'Адрес',
-    is_active BOOLEAN DEFAULT TRUE COMMENT 'Статус',
-    org_id    INTEGER NOT NULL COMMENT 'Индефикатор организации',
+    is_active BOOLEAN          DEFAULT TRUE COMMENT 'Статус',
+    org_id    INTEGER NOT NULL COMMENT 'Идентификатор организации',
     CONSTRAINT fk_organization_office FOREIGN KEY (org_id)
         REFERENCES Organization (id)
 );
@@ -47,7 +59,7 @@ COMMENT ON TABLE Office IS 'Офис';
 CREATE TABLE IF NOT EXISTS User
 (
     id               INTEGER     NOT NULL COMMENT 'Уникальный идентификатор пользователя' PRIMARY KEY AUTO_INCREMENT,
-    version          INTEGER NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
+    version          INTEGER     NOT NULL DEFAULT 0 COMMENT 'Служебное поле hibernate',
     office_id        INTEGER     NOT NULL COMMENT 'Офис',
     CONSTRAINT fk_person_office FOREIGN KEY (office_id)
         REFERENCES Office (id),
@@ -56,14 +68,12 @@ CREATE TABLE IF NOT EXISTS User
     middle_name      VARCHAR(25) COMMENT 'Отчество',
     position         VARCHAR(25) NOT NULL COMMENT 'Должнсть',
     phone            CHAR(10) COMMENT 'Номер телефона',
-    doc_code         INTEGER COMMENT 'Тип документа',
-    CONSTRAINT fk_user_doc_code FOREIGN KEY (doc_code)
-        REFERENCES Document (code),
-    doc_number       VARCHAR(25) COMMENT 'Номер документа',
-    doc_date         DATE COMMENT 'Дата выдачи документа',
+    doc_id           INTEGER COMMENT 'Документ',
+    CONSTRAINT fk_user_doc FOREIGN KEY (doc_id)
+        REFERENCES Document (id),
     citizenship_code INTEGER COMMENT 'Код страны',
     CONSTRAINT fk_user_citizenship_code FOREIGN KEY (citizenship_code)
-        REFERENCES Country (code),
-    is_identified    BOOLEAN DEFAULT TRUE COMMENT 'Статус'
+        REFERENCES Country (id),
+    is_identified    BOOLEAN              DEFAULT TRUE COMMENT 'Статус'
 );
 COMMENT ON TABLE User IS 'Пользователь';
